dfcd22ba7db2a512f3d7bd32aa9a290e9bcd7802
DevGibby <71801512+DevGibby@users.noreply.github.com>
2022-04-05 20:32:50 -0400

    simplified if statements

--- src/pages/coin/components/Stats.js
+++ src/pages/coin/components/Stats.js
@@ -10,34 +10,10 @@ export const Stats = ({coin}) => {
   return (
     <StyledStats>
         <div className="change-container">
-          {
-            Number(coin.delta_1h) < 0 ? (
-              <h3 id="red"><span>1hr:</span> {Number(coin.delta_1h)}%</h3>
-            ): (
-              <h3><span>1hr:</span> {Number(coin.delta_1h)}%</h3>
-            )
-          }
-          {
-            Number(coin.delta_24h) < 0 ? (
-              <h3 id="red"><span>24hr:</span> {Number(coin.delta_24h)}%</h3>
-            ): (
-              <h3><span>24hr:</span> {Number(coin.delta_24h)}%</h3>
-            )
-          }
-          {
-            Number(coin.delta_7d) < 0 ? (
-              <h3 id="red"><span>7d:</span> {Number(coin.delta_7d)}%</h3>
-            ): (
-              <h3><span>7d:</span> {Number(coin.delta_7d)}%</h3>
-            )
-          }
-          {
-            Number(coin.delta_30d) < 0 ? (
-              <h3 id="red"><span>30d:</span> {Number(coin.delta_30d)}%</h3>
-            ): (
-              <h3><span>30d:</span> {Number(coin.delta_30d)}%</h3>
-            )
-          }
+          <h3 style={{color: Number(coin.delta_1h) < 0 ? 'red' : ''}}><span>1h:</span> {Number(coin.delta_1h)}%</h3>
+          <h3 style={{color: Number(coin.delta_24h) < 0 ? 'red' : ''}}><span>24hr:</span> {Number(coin.delta_24h)}%</h3>
+          <h3 style={{color: Number(coin.delta_7d) < 0 ? 'red' : ''}}><span>7d:</span> {Number(coin.delta_7d)}%</h3>
+          <h3 style={{color: Number(coin.delta_30d) < 0 ? 'red' : ''}}><span>30d:</span> {Number(coin.delta_30d)}%</h3>
         </div>
         <div className="stats-container">
             <h3><span>High 24hr:</span> ${Number(coin.high_24h).toFixed(2)}</h3>
@@ -59,9 +35,6 @@ max-width: 600px;
     span {
       color: ${palette.subtitleColor};
     }
-  }
-  #red {
-    color: red;
   }
 
 `;
