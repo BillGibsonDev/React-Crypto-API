e5fb7b7ce567fe05f36305a6b9e10e3d3f778d40
DevGibby <71801512+DevGibby@users.noreply.github.com>
2022-04-05 20:33:03 -0400

    Update Stats.js

--- src/pages/coin/components/Stats.js
+++ src/pages/coin/components/Stats.js
@@ -16,9 +16,9 @@ export const Stats = ({coin}) => {
           <h3 style={{color: Number(coin.delta_30d) < 0 ? 'red' : ''}}><span>30d:</span> {Number(coin.delta_30d)}%</h3>
         </div>
         <div className="stats-container">
-            <h3><span>High 24hr:</span> ${Number(coin.high_24h).toFixed(2)}</h3>
-            <h3><span>Volume 24hr:</span> ${shortNum(Number(coin.total_volume_24h).toFixed(0))}</h3>
-            <h3><span>Market Cap:</span> ${shortNum(Number(coin.market_cap).toFixed(0))}</h3>
+          <h3><span>High 24hr:</span> ${Number(coin.high_24h).toFixed(2)}</h3>
+          <h3><span>Volume 24hr:</span> ${shortNum(Number(coin.total_volume_24h).toFixed(0))}</h3>
+          <h3><span>Market Cap:</span> ${shortNum(Number(coin.market_cap).toFixed(0))}</h3>
         </div>
     </StyledStats>
   )
